fc7826b17bf4af17e6eb7181471f46cd5504da4b
YongHun Lee <yosiyong@gmail.com>
2024-06-20 00:08:16 +0900

    purchase click modal

--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -2,9 +2,39 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { BookType } from "../types/types";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+type BookProps = {
+  book: BookType;
+};
 
 // eslint-disable-next-line react/display-name
-const Book = ({ book }: any) => {
+const Book = ({ book }: BookProps) => {
+  const [showModal, setShowModal] = useState(false);
+  const {data:session} = useSession();
+  const user = session?.user;
+  const router = useRouter();
+
+  const handlePurchaseClick = () => {
+    setShowModal(true);
+  }
+
+  const handleCancel = () => {
+    setShowModal(false);
+  }
+
+  const handlePurchaseConfirm = () => {
+    if (!user) {
+      setShowModal(false);
+      //ログインページへリダイレクト
+      router.push("/login");
+    }else{
+      //決済
+    }
+  }
+
   return (
     <>
       {/* アニメーションスタイル */}
@@ -25,10 +55,10 @@ const Book = ({ book }: any) => {
       `}</style>
 
       <div className="flex flex-col items-center m-4">
-        <a className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none">
+        <a onClick={handlePurchaseClick} className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none">
           <Image
             priority
-            src={book.thumbnail}
+            src={book.thumbnail.url}
             alt={book.title}
             width={450}
             height={350}
@@ -40,18 +70,19 @@ const Book = ({ book }: any) => {
             <p className="mt-2 text-md text-slate-700">値段：{book.price}</p>
           </div>
         </a>
-
-        {/* <div className="absolute top-0 left-0 right-0 bottom-0 bg-slate-900 bg-opacity-50 flex justify-center items-center modal">
-          <div className="bg-white p-8 rounded-lg">
-            <h3 className="text-xl mb-4">本を購入しますか？</h3>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">
-              購入する
-            </button>
-            <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-              キャンセル
-            </button>
+        {showModal && (
+            <div className="absolute top-0 left-0 right-0 bottom-0 bg-slate-900 bg-opacity-50 flex justify-center items-center modal">
+            <div className="bg-white p-8 rounded-lg">
+              <h3 className="text-xl mb-4">本を購入しますか？</h3>
+              <button onClick={handlePurchaseConfirm} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">
+                購入する
+              </button>
+              <button onClick={handleCancel} className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+                キャンセル
+              </button>
+            </div>
           </div>
-        </div> */}
+        )}
       </div>
     </>
   );
