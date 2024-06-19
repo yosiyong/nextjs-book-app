aa524f19c2d64d0e97765a279490031f6243eb8b
YongHun Lee <yosiyong@gmail.com>
2024-06-20 00:24:30 +0900

    logout

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -24,11 +24,18 @@ const Header = () => {
             ホーム
           </Link>
           <Link
-            href="/login"
+            href={user ? "/profile" : "/login"}
             className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
           >
-            ログイン
+            {user ? "プロフィール" : "ログイン"}
           </Link>
+
+          {user ? (
+              <button onClick={()=>signOut({callbackUrl:"/login"})} 
+              className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">ログアウト</button>
+          ) : (
+            ""
+          )}
 
           <Link href={`/profile`}>
             <Image
