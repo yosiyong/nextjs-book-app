40ec80578ad5704e6812e152d9841d883aa7f05f
YongHun Lee <yosiyong@gmail.com>
2024-06-24 09:03:05 +0900

    prisma deploy error

---
+++ app/api/checkout/success/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request, res: Response) {
+    try {
+
+    } catch (err) {
+        return NextResponse.json(err);
+    }
+    
+}
